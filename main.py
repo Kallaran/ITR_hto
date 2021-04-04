784edb9b55e72b0afc6fc00b0c6d1c1b0fc13c42
Jean <jean.derieux@gmail.com>
2021-04-04 04:34:29 +0200

    Add Earliest Deadline First

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import svgwrite
 
 from scheduling_visualization import display_tasks_names, display_graduated_lines, display_tasks_arrival, display_tasks_deadlines
-from schedulers import rateMonotonic
+from schedulers import rateMonotonic, earliestDeadlineFirst
 
 LIST_FUNC = ["navigation" , "Detection_contact" , "Detection_distance"]
 FirstArrival = [0, 0, 0]
-Pi = [8, 5, 10]
-Di = [4, 5, 10]
-Ci = [3, 2, 2]
+Pi = [20, 5, 10]
+Ci = [1, 2, 4]
+Di = [8, 4, 10]
 
 
 
@@ -17,12 +17,11 @@ dwg = svgwrite.Drawing('test.svg', profile='tiny')
 display_tasks_names(dwg, LIST_FUNC)
 display_graduated_lines(dwg, LIST_FUNC, Pi)
 
-rateMonotonic(dwg, LIST_FUNC, Pi, FirstArrival, Ci, Di)
-
+#rateMonotonic(dwg, LIST_FUNC, Pi, FirstArrival, Ci, Di)
+earliestDeadlineFirst(dwg, LIST_FUNC, Pi, FirstArrival, Ci, Di)
 
 display_tasks_arrival(dwg, LIST_FUNC, Pi, FirstArrival)
 display_tasks_deadlines(dwg, LIST_FUNC, Pi, FirstArrival, Di)
-
 
 
 dwg.save()
--- schedulers.py
+++ schedulers.py
@@ -18,7 +18,8 @@ def shorterPeriod(Pi):
 
 
 
-
+#Tasks with shorter periods will have highter priorities
+#In case of identical period, the deterministic tie-breaking rule is to give higher priority to the first elements of LIST_FUNC
 def rateMonotonic(dwg, LIST_FUNC, Pi, FirstArrival, Ci, Di):
 
     hyper_periode = ppcm(*Pi)
@@ -50,7 +51,7 @@ def rateMonotonic(dwg, LIST_FUNC, Pi, FirstArrival, Ci, Di):
             #if this times there is a new task arrival then reset reset new_Pi[count], update new_Ci[count] and update new_FirstArrival[count]
             if new_FirstArrival[count] == times:
                 new_Pi[count] = Pi[count]
-                new_Ci[count] = new_Ci[count] + Ci[count]
+                new_Ci[count] = new_Ci[count] + Ci[count] #nomber of late squares + addition of squares 
                 new_Di[count] = new_FirstArrival[count] + Di[count]
                 new_FirstArrival[count] = new_FirstArrival[count] + Pi[count]
                 
@@ -75,8 +76,8 @@ def rateMonotonic(dwg, LIST_FUNC, Pi, FirstArrival, Ci, Di):
                 )
 
 
-                new_Ci[count] = new_Ci[count] - 1
-                if new_Ci[count] == 0:
+                new_Ci[count] = new_Ci[count] - 1 #a square has been placed, we reduce the number of squares that remain to be placed
+                if new_Ci[count] == 0:        #if for a task all of its squares have been placed then it is prevented from being selected again 
                     new_Pi[count] = -1
 
                 square = 1
@@ -85,6 +86,94 @@ def rateMonotonic(dwg, LIST_FUNC, Pi, FirstArrival, Ci, Di):
 
         x_start_rect = x_start_rect + 20
 
+
+
+def earlierAbsoluteDeadline(Di):
+    earlierAbsoluteDeadline = -1
+
+    for d in Di:
+        if d != -1:
+            earlierAbsoluteDeadline = d
+
+    for d in Di:
+        if earlierAbsoluteDeadline > d and d != -1:
+            earlierAbsoluteDeadline = d
+
+    return earlierAbsoluteDeadline
+
+
+
+
+#Tasks with earlier absolute deadlines will be executed at higher
+#priorities (deterministic tie-breaking rule in case of equality)
+def earliestDeadlineFirst(dwg, LIST_FUNC, Pi, FirstArrival, Ci, Di):
+
+
+    hyper_periode = ppcm(*Pi)
+    new_FirstArrival = FirstArrival[:]
+    new_Ci = Ci[:]
+    new_Di = Di[:]
+    
+    #to set each element of new_Ci to 0, we will fill it with each task arrival
+    for count, e in enumerate(new_Ci):
+        new_Ci[count] = 0
+
+
+
+    x_start_rect = 100.5
+
+    for times in range(hyper_periode):
+        
+        #boolean to know if we have already added a square (1) or not yet(0)
+        square = 0
+
+        #we iterate one time to update if needed
+        for count, fun in enumerate(LIST_FUNC):
+            
+            #check if a deadline is outdated
+            if new_Di[count] == times and new_Ci[count] != 0:
+                    print( LIST_FUNC[count] , "misses its deadline at : ", times )
+            
+            #if this times there is a new task arrival then reset reset new_Pi[count], update new_Ci[count] and update new_FirstArrival[count]
+            if new_FirstArrival[count] == times:
+                new_Ci[count] = new_Ci[count] + Ci[count] #nomber of late squares + addition of squares 
+                new_Di[count] = new_FirstArrival[count] + Di[count]
+                new_FirstArrival[count] = new_FirstArrival[count] + Pi[count]
+                
+                
+
+        for count, fun in enumerate(LIST_FUNC):
+
+            y_start_rect = 45
+
+
+            if new_Di[count] == earlierAbsoluteDeadline(new_Di) and new_Ci[count] != 0 and square == 0:
+
+                y_start_rect = y_start_rect + (count * 100) 
+                dwg.add(dwg.rect((x_start_rect,y_start_rect), (19.5, 19.5), 2, 2, fill='yellow'))
+                
+                dwg.add(dwg.text(times,
+                    insert=(x_start_rect + 5, y_start_rect + 10),
+                    stroke='none',
+                    fill='black',
+                    font_size='8px',
+                    font_weight="bold",
+                    font_family="Arial")
+                )
+
+
+                new_Ci[count] = new_Ci[count] - 1     #a square has been placed, we reduce the number of squares that remain to be placed
+
+
+                if new_Ci[count] == 0:        #if for a task all of its squares have been placed then it is prevented from being selected again 
+                    new_Di[count] = -1
+
+
+                square = 1
+
+            
+
+        x_start_rect = x_start_rect + 20
 
 
 
